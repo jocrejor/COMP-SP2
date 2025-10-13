07df930a5079c863282ca29027e712eb38c1aad0
david <davsermel@gmail.com>
2025-10-13 09:30:09 +0200

    llevar archius innecesaris

--- Attributs/Act2.4/act2-4.js
+++
@@ -1,5 +0,0 @@
-
-let persona = null;
-persona = {nombre: "Pepe", edad: "30", altura:178};
-
-console.log("El usuario " + persona.nombre + " tiene " + persona.edad + " años y mide " + persona.altura + " cm ");
