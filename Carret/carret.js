30d46a6c3cdbefef6ad774632d43b51a4a03f0a8
“[ViictorAC]” <[victorach007@gmail.com]>
2025-10-22 13:27:44 +0200

    creacio funcio mostrar productes

--- Carret/carret.js
+++ Carret/carret.js
@@ -1,3 +1,46 @@
+const productes = [
+    {
+        id: 1,
+        name: "Philips Bombilla LED",
+        price: 6.99,
+        description: "Bombilla LED de alta eficiencia, casquillo E27, ideal para sustituir incandescentes. Luz cálida y consumo reducido.",
+        image: "https://tse1.mm.bing.net/th/id/OIP.gFS6_FPcJxXnhKYdo1xAdgHaHs?pid=Api"
+    },
+    {
+        id: 2,
+        name: "Salamina",
+        price: 64.95,
+        description: "Ventilador de Techo Silencioso Salamina Blanco 132cm Motor DC",
+        image: "https://www.efectoled.com/957216-thickbox_sklum/ventilador-de-techo-silencioso-salamina-blanco-132cm-motor-dc.jpg"
+    },
+    {
+        id: 3,
+        name: "Interruptor Conmutador",
+        price: 330.95,
+        description: "Interruptor Conmutador de Transferencia automática de redes 4-P 125A. MAXGE",
+        image: "https://www.efectoled.com/1559785-thickbox_sklum/interruptor-conmutador-de-transferencia-automatica-de-redes-4-p-125a-maxge.jpg"
+    },
+    {
+        id: 4,
+        name: "Cable Eléctrico Plano",
+        price: 1.25,
+        description: "Cable Eléctrico Plano Manguera 4x0.5mm² para Tiras LED RGB a metros",
+        image: "https://www.efectoled.com/1501000-thickbox_sklum/cable-electrico-plano-manguera-4x05mm2-para-tiras-led-rgb.jpg"
+    },
+    {
+        id: 5,
+        name: "Plafón LED 24W Cuadrado",
+        price: 24.95,
+        description: "Plafón LED 24W Cuadrado SwitchCCT Seleccionable 420x420 mm Doble Cara",
+        image: "https://www.efectoled.com/1088951-thickbox_sklum/plafon-led-24w-cuadrado-cct-420x420-mm-doble-cara-switchcct.jpg"
+    }
+];
+
+
+
+
+
+
 
 // Función principal para mostrar el carrito y gestionar las acciones
 function mostrarCarret() {
@@ -67,10 +110,9 @@ function eliminar(index) {
     mostrarCarret(); // Se actualiza la vista del carrito
 }
 
-// Muestra el carrito automáticamente cuando la página termina de cargarse
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('productes-container');
 
+function mostrarProductes() {
+    const container = document.getElementById('productes-container');
     productes.forEach(p => {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -84,45 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
         container.appendChild(div);
     });
-});
-
-const productes = [
-    {
-        id: 1,
-        name: "Philips Bombilla LED",
-        price: 6.99,
-        description: "Bombilla LED de alta eficiencia, casquillo E27, ideal para sustituir incandescentes. Luz cálida y consumo reducido.",
-        image: "https://tse1.mm.bing.net/th/id/OIP.gFS6_FPcJxXnhKYdo1xAdgHaHs?pid=Api"
-    },
-    {
-        id: 2,
-        name: "Salamina",
-        price: 64.95,
-        description: "Ventilador de Techo Silencioso Salamina Blanco 132cm Motor DC",
-        image: "https://www.efectoled.com/957216-thickbox_sklum/ventilador-de-techo-silencioso-salamina-blanco-132cm-motor-dc.jpg"
-    },
-    {
-        id: 3,
-        name: "Interruptor Conmutador",
-        price: 330.95,
-        description: "Interruptor Conmutador de Transferencia automática de redes 4-P 125A. MAXGE",
-        image: "https://www.efectoled.com/1559785-thickbox_sklum/interruptor-conmutador-de-transferencia-automatica-de-redes-4-p-125a-maxge.jpg"
-    },
-    {
-        id: 4,
-        name: "Cable Eléctrico Plano",
-        price: 1.25,
-        description: "Cable Eléctrico Plano Manguera 4x0.5mm² para Tiras LED RGB a metros",
-        image: "https://www.efectoled.com/1501000-thickbox_sklum/cable-electrico-plano-manguera-4x05mm2-para-tiras-led-rgb.jpg"
-    },
-    {
-        id: 5,
-        name: "Plafón LED 24W Cuadrado",
-        price: 24.95,
-        description: "Plafón LED 24W Cuadrado SwitchCCT Seleccionable 420x420 mm Doble Cara",
-        image: "https://www.efectoled.com/1088951-thickbox_sklum/plafon-led-24w-cuadrado-cct-420x420-mm-doble-cara-switchcct.jpg"
-    }
-];
+}
 
 function afegirAlCarret(id) {
     const producte = productes.find(p => p.id === id);
