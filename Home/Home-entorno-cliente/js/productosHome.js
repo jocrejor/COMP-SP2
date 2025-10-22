e2b31223c527f64a413c14c76c9937eb8a316211
Adria <s.adria.navarro@gmail.com>
2025-10-22 13:35:01 +0200

    Test

---
+++ Home/Home-entorno-cliente/js/productosHome.js
@@ -0,0 +1,36 @@
+//Familias que existen para asi poder ser filtradas en la home
+const families = [
+  { id: 1, name: "Tiras LED" },
+  { id: 2, name: "Bombillas LED" },
+  { id: 3, name: "Paneles LED" }
+];
+
+//Productos
+const products = [
+  {
+    id: 1,
+    name: "Tira LED RGB",
+    price: 19.99,
+    description: "Tira flexible RGB perfecta para decorar habitaciones, techos o muebles.",
+    image: "img/strip.jpg",
+    family_id: 1
+  },
+  {
+    id: 2,
+    name: "Bombilla LED cálida",
+    price: 9.99,
+    description: "Bombilla LED de bajo consumo con luz cálida, ideal para interiores.",
+    image: "img/bulb.jpg",
+    family_id: 2
+  },
+  {
+    id: 3,
+    name: "Panel LED blanco",
+    price: 29.99,
+    description: "Panel LED ultrafino con luz blanca brillante, perfecto para oficinas.",
+    image: "img/panel.jpg",
+    family_id: 3
+  }
+];
+
+
