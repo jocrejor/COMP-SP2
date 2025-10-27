aa39535390bf46a2f819a72ef146691db710c702
LukasTamulaitisV <lukastamulaitisv@gmail.com>
2025-10-27 13:39:31 +0100

    BorrarARchivos

--- Home/Ex2-4.js
+++
@@ -1,8 +0,0 @@
-const Persona = {
-    Nombre: "Pepe",
-    Edad: 30,
-    Altura: 178
-};
-
-console.log("El usuario " + Persona.Nombre + " tiene " + Persona.Edad + " años y mide " + Persona.Altura + " centímetros");
-
