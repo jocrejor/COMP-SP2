8cf8e1d2f54325d5ff5e5f72bf77c29641d59364
Manuel Pop <manpop2@alu.edu.gva.es>
2025-10-27 13:23:44 +0100

    canviat js de dades

--- webcorporativa/admin/usuaris/usuaris.js
+++ webcorporativa/admin/usuaris/usuaris.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded"), llistaUsuaris()
 function llistaUsuaris() {
     window.localStorage.getItem("usuaris");
+    if(!localStorage.usuaris){
+        const usuaris = window.User;
+        localStorage.setItem("usuaris", JSON.stringify(usuaris));
+    }
+    const taulaUsuaris = document.getElementById("taulaUsuaris");
+    
 }
