db393df7800c596fd46296eaddc273f0179f8b22
[Alex] <[alexsivera.1999@gmail.com]>
2025-10-29 22:31:10 +0100

    atributos creados

--- Productes/atributos.js
+++ Productes/atributos.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
+    console.log("Pàgina de atributs del producte carregada");
+    
     const btnVolver = document.getElementById("btnVolver");
     btnVolver.addEventListener("click", () => {
         window.location.href = "index.html";
