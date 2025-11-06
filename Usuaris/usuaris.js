f4c1e66a2396e0ab42e68fff4f4de80ec9caa847
MikeyN-ai <migcabgar@alu.edu.gva.es>
2025-11-06 23:36:39 +0100

    Usuaris i Rols Acabats

--- Usuaris/usuaris.js
+++ Usuaris/usuaris.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", main);
 
-// Array per emmagatzemar els usuaris.
+// Array per emmagatzemar els usuaris i rols
 let usuaris = [];
 let rols = [];
-let paginaActual;
+let llistaActual = [];
+
+// Pàgines
+let paginaActual = 1;
+const usuarisPerPagina = 10;
 
 // Funció per iniciar els esdeveniments de la pàgina
 function main() { 
@@ -20,9 +24,10 @@ function main() {
     const botoFiltrarRol = document.getElementById("filtrar_rols");
 
     // Obtencio dels botons de les pàgines
+    const primeraPagina = document.getElementById("primeraPagina");
     const paginaAnterior= document.getElementById("paginaAnterior");
     const paginaSeguent= document.getElementById("paginaSeguent");
-    const pagina = document.getElementById("pagina");
+    const ultimaPagina = document.getElementById("ultimaPagina");
 
     // Configuració del botó per a tornar al formulari d'altaUsuaris
     usuarisButton.addEventListener("click", (e) => {
@@ -34,15 +39,6 @@ function main() {
         window.location.href='rols.html';
     });
 
-    // Configuració de les pàgines
-    paginaAnterior.addEventListener("click", (e) => {
-        paginaActual -= paginaActual;
-    });
-
-    paginaSeguent.addEventListener("click", (e) => {
-        paginaActual += paginaActual;
-    });
-
     rols = JSON.parse(localStorage.getItem('rols')) || Rol;
     usuaris = JSON.parse(localStorage.getItem('usuaris')) || User;
     
@@ -50,17 +46,53 @@ function main() {
 
     // Filtre per nom
     botoBuscarNom.addEventListener("click", () => {
+        paginaActual = 1; 
         const text = buscarNomInput.value.toLowerCase();
         mostrarUsuaris(usuaris.filter(u => u.name.toLowerCase().includes(text)));
     });
 
     // Filtre per rol
     botoFiltrarRol.addEventListener("click", () => {
+        paginaActual = 1; 
         const rolSeleccionat = filtreRol.value;
         if (rolSeleccionat === "") {
             mostrarUsuaris(usuaris);
         } else {
             mostrarUsuaris(usuaris.filter(u => u.rol_id == rolSeleccionat));
+        }
+    });
+
+    // Anar a la primera página
+    primeraPagina.addEventListener("click", () => {
+        if (paginaActual !== 1) {
+            paginaActual = 1;
+            mostrarUsuaris();
+        }
+    });
+
+    // Anar a la pàgina anterior
+    paginaAnterior.addEventListener("click", () => {
+        if (paginaActual > 1) {
+            paginaActual--;
+            mostrarUsuaris();
+        }
+    });
+
+    // Anar a la seguent pàgina
+    paginaSeguent.addEventListener("click", () => {
+        const totalPagines = parseInt((usuaris.length + usuarisPerPagina - 1) / usuarisPerPagina);
+        if (paginaActual < totalPagines) {
+            paginaActual++;
+            mostrarUsuaris();
+        }
+    });
+
+    // Anar a la última pàgina
+    ultimaPagina.addEventListener("click", () => {
+        const totalPagines = parseInt((usuaris.length + usuarisPerPagina - 1) / usuarisPerPagina);
+        if (paginaActual !== totalPagines) {
+            paginaActual = totalPagines;
+            mostrarUsuaris();
         }
     });
     
@@ -69,21 +101,21 @@ function main() {
 
 // Funció que s'encarrega de carregar el select amb els rols per a filtrar
 function carregarRolsSelect() {
-    const filtreRol = document.getElementById("filtreRol");
-    filtreRol.replaceChildren(); // Netejar per si de cas
+    const selectorRol = document.getElementById("filtreRol");
+    selectorRol.replaceChildren(); // Netejar per si de cas
 
     // Opció per mostrar tots
     const optTots = document.createElement("option");
-    optTots.value = "";
-    optTots.textContent = "Tots";
-    filtreRol.appendChild(optTots);
+    optTots.setAttribute("value", "");
+    optTots.appendChild(document.createTextNode("Tots"));
+    selectorRol.appendChild(optTots);
 
     // Afegim cada rol real
-    rols.forEach(r => {
-        const opt = document.createElement("option");
-        opt.value = r.id;          // Guardem la ID del rol
-        opt.textContent = r.name;  // Mostrem el nom del rol
-        filtreRol.appendChild(opt);
+    rols.forEach(rol => {
+        const option = document.createElement("option");
+        option.setAttribute("value", rol.id);
+        option.appendChild(document.createTextNode(rol.name));
+        selectorRol.appendChild(option);
     });
 }
 
@@ -102,6 +134,19 @@ function mostrarUsuaris(llistaFiltrada = usuaris) {
         return;
     }
 
+    // Paginació 
+    const totalPagines = parseInt((llistaFiltrada.length + usuarisPerPagina - 1) / usuarisPerPagina);
+    if (paginaActual > totalPagines) paginaActual = totalPagines;
+
+    // Aplicar paginació
+    const inici = (paginaActual - 1) * usuarisPerPagina;
+    const final = inici + usuarisPerPagina;
+    llistaActual = llistaFiltrada.slice(inici, final);
+
+    // Actualitzar número de pàgina
+    const pagina = document.getElementById("pagina");
+    pagina.setAttribute("value", paginaActual);
+
     // Crear taula
     const taula = document.createElement('table');
     taula.id = 'taula_usuaris';
@@ -117,7 +162,7 @@ function mostrarUsuaris(llistaFiltrada = usuaris) {
     taula.appendChild(header);
 
     // Crear files per a cada usuari
-    llistaFiltrada.forEach(usuari => {
+    llistaActual.forEach(usuari => {
         const fila = document.createElement('tr');
 
         // Camps bàsics (adaptats a les claus en anglès)
